37b12445649f02386e36471f32dc3ec8013e540a
pmuthukrishan <134526949+pmuthukrishan@users.noreply.github.com>
2023-05-27 21:38:59 +0200

    Update Sample1.sql

--- Objects/Sample1.sql
+++ Objects/Sample1.sql
@@ -1,1 +1,20 @@
 
+CREATE TABLE DEPARTMENT
+(
+   DEPTCODE   INT(10),
+   DeptName   CHAR(30),
+   LOCATION   VARCHAR(33)
+);
+
+CREATE TABLE EMPLOYEE
+(
+   EmpCode      INT(4),
+   EmpFName     VARCHAR(15),
+   EmpLName     VARCHAR(15),
+   Job          VARCHAR(45),
+   Manager      CHAR(4),
+   HireDate     DATE,
+   Salary       INT(6),
+   Commission   INT(6),
+   DEPTCODE     INT(2)
+);
